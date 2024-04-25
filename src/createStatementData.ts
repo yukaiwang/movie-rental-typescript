6ee017bfafd0a182778e66d4a1f8f84d2e644b5a
Yukai Wang <yukai.wang1991@gmail.com>
2024-04-26 00:30:19 +0200

    move createStatementData to its own file

---
+++ src/createStatementData.ts
@@ -0,0 +1,60 @@
+export function createStatementData(invoice: any, movies: any): string {
+    const statementData = {} as any;
+    statementData.customer = invoice.customer;
+    statementData.rentals = invoice.rentals.map(enrichRental);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalFrequentRenterPoints = totalFrequentRenterPoints(statementData);
+    return statementData;
+
+    function enrichRental(rental: any) {
+        const result = { ...rental };
+        result.movie = movieFor(result);
+        result.amount = amountFor(result);
+        result.frequentRenterPoints = frequentRenterPointsFor(result);
+        return result;
+    }
+
+    function movieFor(rental: any) {
+        return movies[rental.movie];
+    }
+
+    function amountFor(rental: any) {
+        let result = 0;
+
+        // determine amounts for rental line
+        switch (rental.movie.category) {
+            case "regular":
+                result += 2;
+                if (rental.daysRented > 2) {
+                    result += (rental.daysRented - 2) * 1.5;
+                }
+                break;
+            case "new release":
+                result += rental.daysRented * 3;
+                break;
+            case "children":
+                result += 1.5;
+                if (rental.daysRented > 3) {
+                    result += (rental.daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    function frequentRenterPointsFor(rental: any) {
+        let result = 1;
+        // add bonus for a two day new release rental
+        if ((rental.movie.category === "new release") && rental.daysRented > 1)
+            result++;
+        return result;
+    }
+
+    function totalAmount(data: any) {
+        return data.rentals.reduce((total: number, rental: any) => total + rental.amount, 0);
+    }
+
+    function totalFrequentRenterPoints(data: any) {
+        return data.rentals.reduce((total: number, rental: any) => total + rental.frequentRenterPoints, 0);
+    }
+}
